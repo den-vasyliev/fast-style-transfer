edb1010701dba342b0f07293dc2da35645f3a4b2
Denys Vasyliev <den@msrn.me>
2019-02-21 23:21:15 +0000

    add app

--- app.py
+++ app.py
@@ -1,60 +1,56 @@
-"""
-Flask Serving
-
-This file is a sample flask app that can be used to test your model with an API.
-
-This app does the following:
-    - Handles uploads and looks for an image file send as "file" parameter
-    - Stores the image at ./images dir
-    - Invokes ffwd_to_img function from evaluate.py with this image
-    - Returns the output file generated at /output
-
-Additional configuration:
-    - You can also choose the checkpoint file name to use as a request parameter
-    - Parameter name: checkpoint
-    - It is loaded from /input
-"""
 import os
-from flask import Flask, send_file, request
-from werkzeug.exceptions import BadRequest
+from flask import Flask, request, redirect, url_for
+from flask import send_from_directory
 from werkzeug.utils import secure_filename
+import subprocess
 
-from evaluate import ffwd_to_img
+MODELS = set(['la_muse', 'rain_princess', 'udnie'])
+UPLOAD_FOLDER = './upload'
+RESULT_FOLDER = './result'
+MODELS_FOLDER = './models'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 app = Flask(__name__)
-
-
-@app.route('/', methods=["POST"])
-def style_transfer():
-    """
-    Take the input image and style transfer it
-    """
-    # check if the post request has the file part
-    input_file = request.files.get('file')
-    if not input_file:
-        return BadRequest("File not present in request")
-
-    filename = secure_filename(input_file.filename)
-    if filename == '':
-        return BadRequest("File name is not present in request")
-    if not allowed_file(filename):
-        return BadRequest("Invalid file type")
-
-    input_filepath = os.path.join('./images/', filename)
-    output_filepath = os.path.join('/output/', filename)
-    input_file.save(input_filepath)
-
-    # Get checkpoint filename from la_muse
-    checkpoint = request.form.get("checkpoint", "la_muse.ckpt")
-    ffwd_to_img(input_filepath, output_filepath, '/input/' + checkpoint, '/gpu:0')
-    return send_file(output_filepath, mimetype='image/jpg')
-
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MODELS_FOLDER'] = MODELS_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',methods=['GET'])
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route('/version',methods=['GET'])
+def version():
+	return "neural-art:v1.0.0"
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            return 'No file'
+        file = request.files['image']
+        model = request.form.get('model')
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        try:
+        	app.config['EVALUATE_COMMAND'] =  ( "python evaluate.py --allow-different-dimensions  \
+        			--checkpoint %s/%s.ckpt --in-path %s --out-path %s" 
+        	% (app.config['MODELS_FOLDER'], model, app.config['UPLOAD_FOLDER'], app.config['RESULT_FOLDER']))
+        	
+        	result_success = subprocess.check_output(app.config['EVALUATE_COMMAND'], shell=True)
+        except subprocess.CalledProcessError as e:
+            return "An error occurred while evaluate"
+
+        return send_from_directory(app.config['RESULT_FOLDER'],
+                               filename)
+    return '''Upload new File</title>'''
